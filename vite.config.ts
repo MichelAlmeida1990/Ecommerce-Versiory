a6ea090a58e97b9448da443d1a77b663c4720076
Michas <michelpaulo06@hotmail.com>
2026-02-10 10:23:12 -0300

    Ajusta base para Vercel e Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Ecommerce-Versiory/',
+      base: process.env.VERCEL ? '/' : '/Ecommerce-Versiory/',
       server: {
         port: 3000,
         host: '0.0.0.0',
