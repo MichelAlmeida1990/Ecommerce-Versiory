1f8baf578ad1c813706b322b1542373e159c3c79
Michas <michelpaulo06@hotmail.com>
2026-07-14 21:03:15 -0300

    chore: versao 2.9.2 (correcoes REFCOM166/186/187/188)

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -6842,7 +6842,7 @@ const AdminDashboard: React.FC<AdminDashboardProps> = ({
           >
             Sair
           </button>
-          <p className="text-white/60 text-xs mt-2">© {new Date().getFullYear()} Versiory Store. Todos os direitos reservados. | <span className="font-bold">Versão 2.9.1 (Estável)</span></p>
+          <p className="text-white/60 text-xs mt-2">© {new Date().getFullYear()} Versiory Store. Todos os direitos reservados. | <span className="font-bold">Versão 2.9.2 (Estável)</span></p>
         </div>
       </footer>
     </div>
